24d9c9bfa5410ff67790887cbfbaf264e5f700af
enzomuzi <enzshadows@gmail.com>
2024-11-01 20:35:51 -0300

    feat-01: Configuração da estrutura do projeto

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,78 @@
 import { useNavigation } from "@react-navigation/native";
-import { Button, StatusBar, Text, View } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
 import { AppNavigationProps } from "../../routes/app.routes";
+import HomeButton from "../../components/HomeButton";
 
-export function Home(){
-    const navigation = useNavigation<AppNavigationProps>();
+export function Home() {
+  const navigation = useNavigation<AppNavigationProps>();
 
-    function handleNavigate(){
-        navigation.navigate('poker_cards')
-    }
-    return(
-        <View style={{flex:1 }}>
-            <Text>Home</Text>
-            <Button title="Navigate"  onPress={handleNavigate} />
-        </View>
-    )
+  function handleNavigate() {
+    navigation.navigate("poker_cards");
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.upperView}>
+        <Text style={{ color: "white", fontSize: 70, fontWeight: "bold" }}>
+          Olá!
+        </Text>
+        <Text style={styles.textUpper}>
+          É um prazer recebê-lo na Pokédex! Explore e descubra tudo sobre seus
+          Pokémon favoritos.
+        </Text>
+        <Image
+          source={require("../../assets/homegif/pokeball.gif")}
+          style={styles.poke}
+        />
+      </View>
+      <View style={styles.lowerView}>
+        <Text style={styles.textLower}>
+          Para acessar as informações sobre os Pokémon, clique no botão abaixo:
+        </Text>
+        <HomeButton title="Continuar" onPress={handleNavigate} />
+      </View>
+    </View>
+  );
 }
+
+// Estilos
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#cd5c5c",
+  },
+  upperView: {
+    flex: 1,
+    backgroundColor: "#cd5c5c",
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  lowerView: {
+    flex: 1,
+    backgroundColor: "#f8f8ff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+  },
+  poke: {
+    height: 150,
+    width: 150,
+    justifyContent: "center",
+  },
+  textLower: {
+    fontSize: 22,
+    color: "black",
+    textAlign: "center",
+    marginBottom: 10,
+    paddingBottom: 10,
+  },
+  textUpper: {
+    fontSize: 24,
+    color: "white",
+    textAlign: "center",
+    marginTop: 35,
+    paddingBottom: 20,
+  },
+});
--- src/pages/Poke/index.tsx
+++ src/pages/Poke/index.tsx
@@ -1,9 +1,30 @@
-import { Text } from "react-native";
+import { ScrollView, Text, TextInput, View, StyleSheet } from "react-native";
 
-export function Poke(){
-    console.log('Poke rendered');
+export function Poke() {
+  console.log("Poke rendered");
 
-    return(
-        <Text>Poke</Text>
-    )
+  return (
+    <View style={style.viewBckgrd}>
+      <TextInput style={style.txtBckgrd} placeholder="Pesquisar" />
+      <ScrollView></ScrollView>
+    </View>
+  );
 }
+
+const style = StyleSheet.create({
+  viewBckgrd: {
+    flex: 1,
+    backgroundColor: "#f8f8ff",
+  },
+  txtBckgrd: {
+    height: 40,
+    borderWidth: 2,
+    borderColor: "black",
+    borderRadius: 20,
+    color: "black",
+    fontSize: 20,
+    textAlign: "center",
+    padding: 10,
+    margin: 10,
+  },
+});
